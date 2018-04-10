3d1ea602e804127709d11a9a0adacde4c3fb6c6b
Genaina <genainarodrigues@gmail.com>
2018-04-10 17:22:52 -0300

    Rails 5.1.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,43 +1,47 @@
+# This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
-
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
-
   def index
     @movies = Movie.all
   end
 
+  def show
+    id = params[:id]
+    @movie = Movie.find(id)
+  end
+
   def new
-    # default: render 'new' template
+    @movie = Movie.new
   end
 
   def create
-    params.permit!
-    @movie = Movie.create!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully created."
+    #@movie = Movie.create!(params[:movie]) #did not work on rails 5.
+    @movie = Movie.create(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created!"
     redirect_to movies_path
   end
 
+  def movie_params
+    params.require(:movie).permit(:title,:rating,:description,:release_date)
+  end
+
   def edit
-    @movie = Movie.find params[:id]
+    id = params[:id]
+    @movie = Movie.find(id)
+    #@movie = Movie.find params[:id]
   end
 
   def update
-    params.permit!
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully updated."
+    #@movie.update_attributes!(params[:movie])#did not work on rails 5.
+    @movie.update(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated!"
     redirect_to movie_path(@movie)
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
+    @movie = Movie.find params[:id]
     @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    flash[:notice] = "#{@movie.title} was deleted!"
     redirect_to movies_path
   end
-
 end
